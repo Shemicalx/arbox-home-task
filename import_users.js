f0ff494ec1c1ea4a374c8e47541f737704632898
Noam Shemi <noamshemi@gmail.com>
2021-06-22 13:29:59 +0300

    Initiated INSERT SQL command

--- import_users.js
+++ import_users.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+
+/*======== USAGE ========
+    <script-path> --file-path=<path> --club-id=<id>
+    
+    e.g - 
+    ./import_users.js --file-path=./assets/jimalaya.xlsx --club-id=2400
+  =======================*/
+
 //To make command like scripts more comfortable to work with
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
@@ -13,7 +21,10 @@ if(argv.filePath && argv.clubId) {
     
     let customers = [];
     let customer = {};
-
+    //Iterate over the excel sheet and create a new customers array
+    //At the moment, it does not check for the name of the column, rather it is
+    //hardcoded, assuming column 'A' is always 'first_name' and so on,
+    //just like in the jimalaya.xlsx file I received
     for (let cell in worksheet) {
         const cellAsString = cell.toString();
         if (cellAsString[1] !== 'r' && cellAsString[1] !== 'm' && cellAsString[1] > 1) {
@@ -33,12 +44,14 @@ if(argv.filePath && argv.clubId) {
                 customer.joined_at = worksheet[cell].v;
                 customer.club_id = argv.clubId;
                 customers.push(customer);
+                console.log(
+                    `INSERT INTO ar_db (${Object.keys(customer)}) VALUES (${Object.values(customer)})`
+                );
                 customer = {};
             }
         }
     }
 
-    console.log(customers);
 } else {
     console.log("Usage: ./import_users.js --file-path=<path> --club-id=<id>");
 }
