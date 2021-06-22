ad412339a24dd8d3bd7452abe172bc30e7b5b3ab
Noam Shemi <noamshemi@gmail.com>
2021-06-22 14:07:55 +0300

    Added email test for primary key and secondary SQL command option

--- import_users.js
+++ import_users.js
@@ -14,7 +14,7 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 //To read excel sheets
 const xlsx = require('xlsx');
-//Test if file path command line argument was passed
+//Test if file path command line arguments were passed
 if(argv.filePath && argv.clubId) {
     const workbook = xlsx.readFile(argv.filePath);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
@@ -35,7 +35,13 @@ if(argv.filePath && argv.clubId) {
                 customer.last_name = worksheet[cell].v;
             }
             if (cellAsString[0] === 'C') {
-                customer.email = worksheet[cell].v;
+                //Check that the email is unique, and abort script if it isn't
+                if(customers.every( c => worksheet[cell].v !== c.email)){
+                    customer.email = worksheet[cell].v;
+                } else {
+                    console.log(`${worksheet[cell].v} is not a unique email! All emails must be unique.`);
+                    process.exit(1);
+                };
             }
             if (cellAsString[0] === 'D') {
                 customer.phone = worksheet[cell].v;
@@ -43,13 +49,24 @@ if(argv.filePath && argv.clubId) {
             if (cellAsString[0] === 'E') {
                 customer.joined_at = worksheet[cell].v;
                 customer.club_id = argv.clubId;
+                //push new customer to the customers array and reset it for the next one
                 customers.push(customer);
-                console.log(
-                    `INSERT INTO ar_db (${Object.keys(customer)}) VALUES (${Object.values(customer)})`
-                );
                 customer = {};
             }
         }
+    }
+    
+    /*Insert all customers from the array.
+    Alternatively, could be done in a single command like so - 
+        console.log(`
+            INSERT INTO ar_db (${Object.keys(customers[0])})
+            VALUES ${customers.map( customer => {
+                return `(${Object.values(customer)})`
+            })}`
+        )
+    */
+    for (let customerToInsert in customers) {
+        console.log(`INSERT INTO ar_db (${Object.keys(customers[0])}) VALUES (${Object.values(customers[customerToInsert])})`);
     }
 
 } else {
